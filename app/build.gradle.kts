274bb6e0db8d53ecdc9bfb566a7ec0df3767d5ef
devcold <admin@devcold.in>
2024-11-19 14:26:00 +0700

    Order libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,15 +37,19 @@ android {
 }
 
 dependencies {
-
+    // base
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
