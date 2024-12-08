c0c4530a4d2f4a250de4eb0dafcd380e81b19a2e
devcold <admin@devcold.in>
2024-12-08 19:14:45 +0700

    Don't log ConnectivityException to Crashlytics

--- app/src/main/java/in/devcold/movica/utils/TimberReleaseTree.kt
+++ app/src/main/java/in/devcold/movica/utils/TimberReleaseTree.kt
@@ -2,6 +2,7 @@ package `in`.devcold.movica.utils
 
 import android.util.Log
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import `in`.devcold.movica.data.remote.ConnectivityException
 import kotlinx.coroutines.CancellationException
 import timber.log.Timber
 
@@ -10,7 +11,7 @@ class TimberReleaseTree : Timber.Tree() {
     override fun isLoggable(tag: String?, priority: Int) = priority >= Log.WARN
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        if (t != null && t !is CancellationException) {
+        if (t != null && t !is CancellationException && t !is ConnectivityException) {
             FirebaseCrashlytics.getInstance().recordException(t)
         }
     }
